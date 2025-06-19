18b18c3cd4a197a5d05eb239d46a62d5d7b7d53f
groupofstars <groupofstars36145@gmail.com>
2025-06-19 00:31:39 -0700

    fix: added download links

--- src/layout/index.js
+++ src/layout/index.js
@@ -6,23 +6,28 @@ import Footer from "./footer";
 import { LinksContext } from "../linksContext";
 
 const MainLayout = () => {
-  const releaseAPI = "https://tymt.com/api/uploads/release.json";
-  const [links, setLinks] = useState([]);
+  // const releaseAPI = "https://tymt.com/api/uploads/release.json";
+  const [links, setLinks] = useState([
+    { "platformName": "Windows", "link": "https://github.com/solarenterprises/tymt-launcher-client-tauri/releases/download/v2.2.2/tymtLauncher_2.2.2_x64_en-US.msi" },
+    { "platformName": "Linux", "link": "https://github.com/solarenterprises/tymt-launcher-client-tauri/releases/download/v2.2.2/tymtLauncher_2.2.2_amd64.AppImage" },
+    { "platformName": "mac-apple", "link": "https://github.com/solarenterprises/tymt-launcher-client-tauri/releases/download/v2.2.2/tymtLauncher_aarch64.app.tar.gz" },
+    { "platformName": "mac-intel", "link": "https://github.com/solarenterprises/tymt-launcher-client-tauri/releases/download/v2.2.2/tymtLauncher_x64.app.tar.gz" }
+  ]);
 
-  useEffect(() => {
-    // GET request
-    axios
-      .get(releaseAPI)
-      .then((response) => {
-        let release = response.data.result.data[0] ? response.data.result.data[0] : {};
-        if (release?.downloadLinks) {
-          setLinks(release?.downloadLinks);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   // GET request
+  //   axios
+  //     .get(releaseAPI)
+  //     .then((response) => {
+  //       let release = response.data.result.data[0] ? response.data.result.data[0] : {};
+  //       if (release?.downloadLinks) {
+  //         setLinks(release?.downloadLinks);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
+  // }, []);
 
   return (
     <LinksContext.Provider value={links}>
